0aece2da8882935797abee4f492e4f96eda4e681
Matheus <matheusramosbi.13@gmail.com>
2024-06-07 18:05:14 -0300

    questions, answer and after-element

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        sky: "0 1px 10px 0px rgba(169, 212, 255, 1)",
+      },
+    },
   },
   plugins: [],
 };
